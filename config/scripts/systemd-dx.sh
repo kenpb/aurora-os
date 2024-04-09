152731aae9e551d59d08736d0a2c1814be7d31d9
kenpb <kpalmab@icloud.com>
2024-04-08 22:22:47 -0600

    feat: enable services

---
+++ config/scripts/systemd-dx.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+set -oue pipefail
+
+systemctl enable docker.socket
+systemctl enable podman.socket
+systemctl enable bluefin-dx-groups.service
+systemctl enable --global bluefin-dx-user-vscode.service
