7a809f9cd7feb0fbeeab3ddcf4de6ae310e6af5d
kenpb <kpalmab@icloud.com>
2024-05-15 11:45:26 -0600

    fix: typo in the swtpm service activation

--- config/scripts/systemd-dx.sh
+++ config/scripts/systemd-dx.sh
@@ -5,5 +5,5 @@ set -oue pipefail
 systemctl enable docker.socket
 systemctl enable podman.socket
 systemctl enable bluefin-dx-groups.service
+systemctl enable swtpm-workaround.service
 systemctl enable --global bluefin-dx-user-vscode.service
-systemctl enable --global swtpm-workaround.service
