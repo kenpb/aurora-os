1c896393aa4565e1ea254cbaf5b2f41794541cb2
kenpb <kpalmab@icloud.com>
2024-05-15 18:50:45 -0600

    fix: trying out the official workaround from bluefin

--- config/scripts/systemd-dx.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-set -oue pipefail
-
-systemctl enable docker.socket
-systemctl enable podman.socket
-systemctl enable bluefin-dx-groups.service
-systemctl enable swtpm-workaround.service
-systemctl enable --global bluefin-dx-user-vscode.service
